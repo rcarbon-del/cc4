d35a6777275dbee802797f4e823defb8a55ee95f
Radge Carbonel <rac4843@students.uc-bcf.edu.ph>
2024-01-24 20:30:08 +0800

    rar

--- MergeSort2.java
+++ MergeSort2.java
@@ -2,22 +2,13 @@ import java.util.Arrays;
 import java.util.Random;
 public class MergeSort2 {
     public static int ctr = 0;
-
     public static void merge(int arr[], int l, int m, int r) {
-        int n1 = m - l + 1, n2 = r - m; ctr++;
+        int n1 = m - l + 1, n2 = r - m, i = 0, j = 0, k = l; ctr++;
 
         int L[] = new int[n1], R[] = new int[n2]; ctr++;
 
-        ctr++;
-        for (int i = 0; i < n1; ++i){ ctr++; ctr++;
-            L[i] = arr[l + i]; ctr++;
-        }
-        ctr++;
-        for (int j = 0; j < n2; ++j){ ctr++; ctr++;
-            R[j] = arr[m + 1 + j]; ctr++;
-        }
-
-        int i = 0, j = 0, k = l; ctr++;
+        System.arraycopy(arr, l, L, 0, n1); ctr++;
+        System.arraycopy(arr, m + 1, R, 0, n2); ctr++;
 
         ctr++;
         for (;i < n1 && j < n2; k++) { ctr++; ctr++;
@@ -36,11 +27,10 @@ public class MergeSort2 {
         }
 
         ctr++;
-        for (;j < n2; j++,k++) { ctr++; ctr++;
+        for (;j < n2; j++, k++) { ctr++; ctr++;
             arr[k] = R[j]; ctr++;
         }
     }
-
     public static void sort(int arr[], int l, int r) {
         if (l < r) {
             int m = (l + r) / 2; ctr++;
@@ -51,19 +41,15 @@ public class MergeSort2 {
             merge(arr, l, m, r); ctr++;
         }
     }
-
     public static void main(String args[]) {
-
         Random rand = new Random();
-        int arr[] = new int[100]; ctr++;
+        int arr[] = new int[1000]; ctr++;
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = rand.nextInt(1000);
         }
 
         sort(arr, 0, arr.length - 1); ctr++;
-
         System.out.println("Sorted array\n" + Arrays.toString(arr) + "\n\nCounter: " + ctr ); ctr++;
-
     }
 }
---
+++ MergeSort5.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+import java.util.Random;
+public class MergeSort5 {
+    public static int ctr = 0;
+    public static void merge(int arr[], int temp[], int l, int m, int r) {
+        int i = l, j = m+1, k = l; ctr++; ctr++;
+        for (; i <= r; i++) { ctr++; ctr++;
+            temp[i] = arr[i]; ctr++;
+        }
+        ctr++;
+        while (i <= m && j <= r) { ctr++; ctr++;
+            if (temp[i] <= temp[j]) {
+                arr[k++] = temp[i++]; ctr++;
+            } else {
+                arr[k++] = temp[j++]; ctr++;
+            }
+        }
+        ctr++;
+        while (i <= m) { ctr++; ctr++;
+            arr[k++] = temp[i++]; ctr++;
+        }
+        ctr++;
+        while (j <= r) { ctr++; ctr++;
+            arr[k++] = temp[j++]; ctr++;
+        }
+    }
+    public static void sort(int arr[], int temp[], int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2; ctr++;
+
+            sort(arr, temp, l, m); ctr++;
+            sort(arr, temp, m + 1, r); ctr++;
+
+            merge(arr, temp, l, m, r); ctr++;
+        }
+    }
+    public static void main(String args[]) {
+        Random rand = new Random();
+        int arr[] = new int[1000]; ctr++;
+        int temp[] = new int[arr.length]; ctr++;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(1000);
+        }
+        sort(arr, temp, 0, arr.length - 1); ctr++;
+        System.out.println("Sorted array\n" + Arrays.toString(arr) + "\n\nCounter: " + ctr ); ctr++;
+    }
+}
